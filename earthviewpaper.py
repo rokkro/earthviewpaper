4ad5931a9d5791576b3c43d44bf2260284bdbcc3
rukkro <darrenew@gmail.com>
2016-08-31 19:01:54 -0500

    Add files via upload

---
+++ earthviewpaper.py
@@ -0,0 +1,58 @@
+import platform, os, random, ctypes
+from urllib.request import urlretrieve
+
+def linuxde(wallid):
+    dir = os.path.expanduser("~") + "/Pictures/earthview/"
+    currentd = os.getcwd() + "/" + wallid + ".jpg"
+    if not os.path.exists(dir): #checks if earthview directory exists
+        print("The earthview dir has been created in your Pictures directory.")
+        os.makedirs(dir)
+    os.rename(currentd, dir + wallid + ".jpg") #moves the file to the earthview directory
+    if (os.environ.get("DESKTOP_SESSION") == 'ubuntu' or os.environ.get("DESKTOP_SESSION") == 'gnome'):  #unity 7, gnome3
+        os.system("gsettings set org.gnome.desktop.background picture-uri file://" + dir + wallid + ".jpg")
+    else:
+        print("not compatible (yet?)")
+
+def win(wallid):
+    dir = os.path.expanduser("~") + "\Pictures\earthview\\"
+    currentd = os.getcwd() + "\\" + wallid + ".jpg" #location of dl'd image in current dir
+    if not os.path.exists(dir): #checks if earthview directory exists
+        print("The earthview dir has been created in your Pictures directory.")
+        os.makedirs(dir)
+    os.rename(currentd, dir + wallid + ".jpg") #moves the file to the earthview directory
+    return dir
+
+def detectOS():
+    if(platform.system()=='Windows'):
+        if(platform.architecture()[0]=='64bit'):
+            directory = win(wallid)
+            ctypes.windll.user32.SystemParametersInfoW(20, 0, directory + wallid + ".jpg", 3) #changes wallpaper on 64bit systems
+        else: #32 bit
+            directory = win(wallid)
+            ctypes.windll.user32.SystemParametersInfoA(20, 0, directory + wallid + ".jpg", 3)
+    elif(platform.system()=='Linux'):
+        linuxde(wallid)
+    else:
+        print("Your system isn't compatible.")
+        quit()
+
+def connectdl(): #attempts to retrieve wallids, splits into a list, randomly picks one, and downloads the image
+    for i in range(0,250000):
+        try:
+            urlretrieve("http://pastebin.com/raw/n9hAzgBQ", "temp.txt")
+            urlid = open("temp.txt").read().split(' ')
+            os.remove("temp.txt")
+            wallid = urlid[random.randrange(0, len(urlid)-1)]
+            urlretrieve("https://earthview.withgoogle.com/download/" + wallid + ".jpg", wallid + ".jpg")
+        except: #will keep attempting to connect until the loop ends (30 seconds?)
+           continue
+        else:
+          return wallid
+    return
+
+wallid = connectdl()
+if(wallid != None):
+    detectOS()
+
+
+
---
+++ urlnabber.py
@@ -0,0 +1,22 @@
+'''
+Url nabber: Checks whether Google has added more URLs/images.
+If a url doesnt return a 404, then it is appended to a list + file.
+Used to create the Pastebin link in the main earthview script.
+Will take a while to check all of the links specified in the loop range.
+'''
+
+from urllib.request import *
+
+urls = []
+for i in range(1000,7500):
+    try:
+        urlretrieve('https://www.gstatic.com/prettyearth/assets/full/' + str(i) + ".jpg")
+    except:
+        continue
+    else:
+        urls.append(i)
+        with open("Output.txt","a") as text_file:
+            text_file.write(str(i) + " ")
+
+print("\nDONE. See Output.txt for the usable url id's.\nUse https://www.gstatic.com/prettyearth/assets/full/ URL-ID .jpg")
+
