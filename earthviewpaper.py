d2f6e69341e9e8ad7772d1082f94154207bdfdda
rukkro <darrenew@gmail.com>
2017-01-23 17:16:36 -0600

    Update earthviewpaper.py

--- earthviewpaper.py
+++ earthviewpaper.py
@@ -10,10 +10,8 @@ def linuxde(wallid):
     os.rename(currentd, dir + wallid + ".jpg") #moves the file to the earthview directory
     de = os.environ.get("DESKTOP_SESSION")
     
-    if de in['ubuntu', 'gnome', 'cinnamon', 'pantheon', 'gnome-classic', 'budgie-desktop', 'default']:  #GTK3 based DE's. 'default' refers to Zorin.
+    if de in['ubuntu', 'gnome', 'cinnamon', 'pantheon', 'gnome-classic', 'budgie-desktop', 'default', 'mate']:  #GTK3 based DE's. 'default' refers to Zorin.
         os.system("gsettings set org.gnome.desktop.background picture-uri file://" + dir + wallid + ".jpg")
-    elif(de == 'mate'):
-        os.system("gsettings set org.mate.background picture-filename " + dir + wallid + ".jpg")
     else:
         print("*Your desktop environment is not supported.\n*You can manually change your wallpaper to " + wallid + ".jpg in your ~/Pictures/earthview/ directory.")
 
